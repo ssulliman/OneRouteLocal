25e9c0ee843871171f2a220eff982247a25b80c1
Ross Maslar <Ross.Maslar@capitalone.com>
2016-07-22 10:29:02 -0400

    test worker_login.html as homepage

--- backend_server.py
+++ backend_server.py
@@ -47,7 +47,7 @@ def show_worker_dashboard():
     else:
         print "NOT A GOOD WORKER DASHBOARD REQUEST\n\n\n\n\n\n"
         flash('NOT A GOOD WORKER DASHBOARD REQUEST')
-        return Response("{}", status=204, mimetype='application/json')
+        return render_template('worker_dashboard.html')
 
 
 @app.route("/get_worker_details", methods=['GET', 'POST'])
