e80af5a3db89ef19ea47fc8f2e2914a834413167
Ross Maslar <Ross.Maslar@capitalone.com>
2016-07-21 20:18:22 -0400

    test worker_login.html as homepage

--- backend_server.py
+++ backend_server.py
@@ -84,4 +84,5 @@ def accept_reservation(task_sid, reservation_sid):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
