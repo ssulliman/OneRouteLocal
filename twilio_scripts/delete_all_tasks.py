d794e08612e600ebe4715cb4c4ec81d93063c913
Ross <maslar.5@osu.edu>
2016-07-25 21:30:23 -0400

    Update delete_all_tasks.py

--- twilio_scripts/delete_all_tasks.py
+++ twilio_scripts/delete_all_tasks.py
@@ -1,3 +1,6 @@
+"""
+Delete all tasks currently in OneRoute Twilio Workspace
+"""
 from twilio.rest import TwilioTaskRouterClient
 account_sid = "ACc9111e5c0efc0644b4e754bf2183c1c2"
 auth_token  = "86f3177a99d46d4cb6237b106be7bd9c"
