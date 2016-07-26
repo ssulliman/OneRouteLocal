185bd0d99c059068cf4862718fa930b554c93a84
Ross Maslar <Ross.Maslar@capitalone.com>
2016-07-26 09:53:18 -0400

    Fixed test_POST.py

--- twilio_scripts/test_POST.py
+++ twilio_scripts/test_POST.py
@@ -1,6 +1,9 @@
 import requests
+from flask import jsonify
 
-task_attributes="{\"product\":\"card\",\"skills\":\"[\"disputes\"]\"}"
+task_attributes = {}
+task_attributes["product"] = "card"
+task_attributes["skills"] = "disputes"
 
 result = requests.post("https://one-route.herokuapp.com/create_task", json=task_attributes)
 
