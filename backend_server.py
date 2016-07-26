bea669b20b8a7f024fa942f0157210f5e41c44c5
Ross Maslar <Ross.Maslar@capitalone.com>
2016-07-26 10:58:05 -0400

    Test task reservation accept

--- backend_server.py
+++ backend_server.py
@@ -83,7 +83,7 @@ def get_worker_details():
 def assignment_callback():
     """Respond to assignment callbacks with empty 200 response"""
 
-    task_sid = request.args.get('TaskSid')
+    task_sid = request.data
 
     # TODO - add/update task/event to mongodb
 
