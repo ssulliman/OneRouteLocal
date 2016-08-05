4661bea5f2c8900c27e5a106339fa4c70b084ca7
Badri Krishnan <Badri.Krishnan@capitalone.com>
2016-08-05 11:16:26 -0400

    Added logging

--- backend_server.py
+++ backend_server.py
@@ -9,6 +9,23 @@ from bson import json_util
 
 #Import Logger
 import logging
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = logging.FileHandler('event.log')
+handler.setLevel(logging.INFO)
+
+# create a logging format
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# add the handlers to the logger
+
+logger.addHandler(handler)
+
 
 # Flask import
 from flask import Flask, flash, Response, jsonify, request, render_template
@@ -98,11 +115,13 @@ def event_callback():
     if(form_dict["EventType"] == "task.created"):
         task_dict = {"task_sid":form_dict["TaskSid"], "task_attributes":form_dict["TaskAttributes"], "task_age":form_dict["TaskAge"], "task_assigment_status":form_dict["TaskAssignmentStatus"], "task_reason":"None" }
         db.tasks.insert(task_dict)
-        print "Task Sid: %s" %(form_dict["TaskSid"])
+        log_message = "Task Sid: %s, Task Attributes: %s, Task Age: %s, Task Status: %s" %(form_dict["TaskSid"],form_dict["TaskAttributes"],form_dict["TaskAge"],form_dict["TaskAssignmentStatus"])
+        print "" %(form_dict["TaskSid"])
         print "Task Attributes: %s" % (form_dict["TaskAttributes"])
         print "Task Age: %s" %(form_dict["TaskAge"])
         print "Task Status: %s" %(form_dict["TaskAssignmentStatus"])
         print "Task Creation"
+        logger.info()
 
     elif(form_dict["EventType"] == "task.cancelled"):
         #TODO-Find the task and update it to cancelled and give the reason why it was cancelled
@@ -143,16 +162,16 @@ def event_callback():
     #Switch on EventType for Reservation Events
     elif(form_dict["EventType"] == "reservation.created"):
         print "Reservation is Created"
-    elif(form_dict["EventType"] == "reservation.accepted"):
 
+    elif(form_dict["EventType"] == "reservation.accepted"):
         print "Making a call to a phone number"
         print "\nReservation is accepted"
         task_sid = form_dict["TaskSid"]
-        task = client.tasks(workspace_sid).get(task_sid)
+        task = task_router.tasks(workspace_sid).get(task_sid)
         print task.attributes
-        #call = voice_client.calls.create(url="http://demo.twilio.com/docs/voice.xml", to="+14083100604", from_="+14082146768")
-        #print(call.sid)
-        
+        call = voice_client.calls.create(url="http://demo.twilio.com/docs/voice.xml", to=task.attributes["phone_number"], from_="+14082146768")
+        print(call.sid)
+
     elif(form_dict["EventType"] == "reservation.rejected"):
         print "Reservation is Rejected"
     elif(form_dict["EventType"] == "reservation.timeout"):
