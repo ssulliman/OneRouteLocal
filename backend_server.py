f80b0819e46f2085561f22e7dd6d87260f7f12a5
Naga Deepak Chelluri <nagadeepakc@gmail.com>
2016-08-17 20:43:36 -0400

    modified data model

--- backend_server.py
+++ backend_server.py
@@ -49,17 +49,18 @@ twilio_client = TwilioRestClient(account_sid, auth_token)
 # DB STUFF
 
 # Create our database model
-class TestTable(db.Model):
-    __tablename__ = "users"
+
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
     email = db.Column(db.String(120), unique=True)
 
-    def __init__(self, email):
+    def __init__(self, name, email):
+        self.name = name
         self.email = email
 
     def __repr__(self):
-        return '<E-mail %r>' % self.email
-
+        return '<Name %r>' % self.name
 
 
 # Define Flask routes
