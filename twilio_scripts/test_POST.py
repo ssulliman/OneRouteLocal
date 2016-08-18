c1af799da461d365e2facce787b68559892b553f
Badri Krishnan <Badri.Krishnan@capitalone.com>
2016-08-18 09:48:49 -0400

    modified test post

--- twilio_scripts/test_POST.py
+++ twilio_scripts/test_POST.py
@@ -1,6 +1,6 @@
 import requests
 from flask import jsonify
-
+import json
 task_attributes = {}
 task_attributes["product"] = "bank"
 task_attributes["skills"] = "disputes"
@@ -10,14 +10,17 @@ task_attributes["customer_name"] = "Badri Krishnan"
 worker_state = {}
 worker_state["state"] = "Busy"
 worker_state["TaskQueueName"] = "Banking"
+'''result = requests.post("https://one-route.herokuapp.com/create_task", json=task_attributes)
 result = requests.post("https://one-route.herokuapp.com/create_task", json=task_attributes)
-result = requests.post("https://one-route.herokuapp.com/create_task", json=task_attributes)
-result = requests.post("https://one-route.herokuapp.com/create_task", json=task_attributes)
+result = requests.post("https://one-route.herokuapp.com/create_task", json=task_attributes)'''
 
 #result = requests.post("https://one-route.herokuapp.com/change_worker_state", json=worker_state)
-result = requests.get("https://one-route.herokuapp.com/get_taskqueue_list", json=worker_state)
+resp = requests.get("https://one-route.herokuapp.com/get_taskqueue_list", json=worker_state)
+task_dict = json.loads(resp.json().get("1"))
+print task_dict["phone_number"]
 
-
-
-print result.text
-print result
+phone_attributes = {}
+phone_attributes["to"] = task_dict["phone_number"]
+phone_attributes["from"] = "+14082146768"
+requests.post("https://one-route.herokuapp.com/make_call", json=phone_attributes)
+#print resp.text
