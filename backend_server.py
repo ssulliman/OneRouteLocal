8d6396c91633fe9d7b0c8a93d7a8c7c335e68f08
Ross Maslar <Ross.Maslar@capitalone.com>
2016-08-18 09:56:27 -0400

    Fix PG db

--- backend_server.py
+++ backend_server.py
@@ -51,9 +51,9 @@ twilio_client = TwilioRestClient(account_sid, auth_token)
 # Create our database model
 
 class User(db.Model):
-    id = PGdb.Column(db.Integer, primary_key=True)
-    name = PGdb.Column(db.String(80))
-    email = PGdb.Column(db.String(120), unique=True)
+    id = PGdb.Column(PGdb.Integer, primary_key=True)
+    name = PGdb.Column(PGdb.String(80))
+    email = PGdb.Column(PGdb.String(120), unique=True)
 
     def __init__(self, name, email):
         self.name = name
